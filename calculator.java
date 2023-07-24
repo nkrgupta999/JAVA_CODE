3a1ffd7041a4dee6099ae36fb1b2a8ecb7b45aa9
nkrgupta999 <nitinkumargupta999@outlook.com>
2023-07-24 12:28:22 +0530

    Calculator_Class_Close

--- calculator.java
+++ calculator.java
@@ -35,39 +35,40 @@ public class calculator {
 				"Select Option According to your choice :-\n1.Addition\n2.Subtraction\n3.Multiplication\n4.DDDivision");
 		int option = sc.nextInt();
 		switch (option) {
-		case 1:
-			System.out.println("Enter your first(1st) number for addition :");
-			int a = sc.nextInt();
-			System.out.println("Enter your second(2nd) number for addition :");
-			int b = sc.nextInt();
-			addition(a, b);
-			break;
-		case 2:
-			System.out.println("Enter your first(1st) number for subtraction :");
-			int c = sc.nextInt();
-			System.out.println("Enter your second(2nd) number for Subtraction :");
-			int d = sc.nextInt();
-			subtraction(c, d);
-			break;
-		case 3:
-			System.out.println("Enter your first(1st) number for Multiplication :");
-			int e = sc.nextInt();
-			System.out.println("Enter your second(2nd) number for Multiplication :");
-			int f = sc.nextInt();
-			addition(e, f);
-			break;
-		case 4:
-			System.out.println("Enter your first(1st) number for Division :");
-			int g = sc.nextInt();
-			System.out.println("Enter your second(2nd) number for Division :");
-			int h = sc.nextInt();
-			addition(g, h);
-			break;
+			case 1:
+				System.out.println("Enter your first(1st) number for addition :");
+				int a = sc.nextInt();
+				System.out.println("Enter your second(2nd) number for addition :");
+				int b = sc.nextInt();
+				addition(a, b);
+				break;
+			case 2:
+				System.out.println("Enter your first(1st) number for subtraction :");
+				int c = sc.nextInt();
+				System.out.println("Enter your second(2nd) number for Subtraction :");
+				int d = sc.nextInt();
+				subtraction(c, d);
+				break;
+			case 3:
+				System.out.println("Enter your first(1st) number for Multiplication :");
+				int e = sc.nextInt();
+				System.out.println("Enter your second(2nd) number for Multiplication :");
+				int f = sc.nextInt();
+				addition(e, f);
+				break;
+			case 4:
+				System.out.println("Enter your first(1st) number for Division :");
+				int g = sc.nextInt();
+				System.out.println("Enter your second(2nd) number for Division :");
+				int h = sc.nextInt();
+				addition(g, h);
+				break;
 
-		default:
-			System.out.println("----------------INVALID CHOICE-----------------");
+			default:
+				System.out.println("----------------INVALID CHOICE-----------------");
 		}
 		System.out.println("------------------THE END-----------------");
+		sc.close();
 	}
 
 }
