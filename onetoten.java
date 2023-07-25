1294b2365d3f67ddfcfde961a55e73ed6896fd79
nkrgupta999 <nitinkumargupta999@outlook.com>
2023-07-25 22:30:42 +0530

    1_to_10

---
+++ onetoten.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class onetoten {
+    public static void main(String args[]) {
+        Scanner ot = new Scanner(System.in);
+        int i = 1;
+        while (i <= 10) {
+            System.out.println("Value for " + i + " loop is :" + i);
+            i++;
+        }
+        ot.close();
+    }
+
+}
