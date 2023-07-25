ca37f64f425d1163f47d47ed0216b346d63de723
nkrgupta999 <nitinkumargupta999@outlook.com>
2023-07-25 23:12:29 +0530

    Square_pattern

--- squarepattern.java
+++ squarepattern.java
@@ -3,10 +3,13 @@ import java.util.*;
 public class squarepattern {
     public static void main(String[] args) {
         Scanner sp = new Scanner(System.in);
-        for (int i = 0; i < 4; i++) {
-            System.out.println(" * * * *");
+        for (int i = 0; i < 4; i++){
+            for(int j=0;j<4;j++){
+                System.out.print("*");
+            }
+            System.out.println(" ");
         }
-        sp.close();
+            sp.close();
 
     }
 }
