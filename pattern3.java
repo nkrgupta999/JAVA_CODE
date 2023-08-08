a6081f8b5b973f7ee7068154bc4976ce14342168
nkrgupta999 <nitinkumargupta999@outlook.com>
2023-08-08 18:34:00 +0530

    Message Upadte

--- pattern3.java
+++ pattern3.java
@@ -1,25 +1,27 @@
+
 //    ****
 //    *  *
 //    *  *
 //    ****
 import java.util.Scanner;
+
 public class pattern3 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter Row and Column :Update ");
+		System.out.println("Enter the number of Row and Column : ");
 		int row = sc.nextInt();
 		int col = sc.nextInt();
-		for (int i = 1; i <=row; i++) {
-			for (int j = 1; j <= col; j++) 
-				if((i==1 || i==col) || (j==1 || j==col))
+		for (int i = 1; i <= row; i++) {
+			for (int j = 1; j <= col; j++)
+				if ((i == 1 || i == col) || (j == 1 || j == col))
 					System.out.print("*");
 				else
 					System.out.print(" ");
-			
+
 			System.out.println();
-			
+
 		}
-        sc.close();
+		sc.close();
 	}
 }
