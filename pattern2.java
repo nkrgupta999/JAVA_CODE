36590e8667ab267b9a41688b4ea0173f036300ad
nkrgupta999 <nitinkumargupta999@outlook.com>
2023-08-08 15:33:32 +0530

    Update Row Insertion Method

--- pattern2.java
+++ pattern2.java
@@ -2,13 +2,18 @@
         //    ******
         //    ******
         //    ******
+import java.util.*;
 public class pattern2 {
     public static void main(String[] args) {
-        for(int i=0;i<6;i++){
+        Scanner sc=new Scanner (System.in);
+        System.out.println("Enter the number of rows : ");
+        int row =sc.nextInt();
+        for(int i=0;i<row;i++){
             for(int j=0;j<6;j++){
                 System.out.print("*");
             }
             System.out.println();
         }
+        sc.close();
     }
 }
