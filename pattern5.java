c5381da59e1658a28d8d030ababa11dbba1870d5
nkrgupta999 <nitinkumargupta999@outlook.com>
2023-08-08 18:47:33 +0530

    Clearify with Output

--- pattern5.java
+++ pattern5.java
@@ -2,7 +2,7 @@ public class pattern5 {
     public static void main(String[] args) {
         for (int i = 0; i <= 4; i++) {
             for (int j = 0; j < 4; j++)
-                System.out.println("*");
+                System.out.print("*");
             System.out.println();
         }
 
